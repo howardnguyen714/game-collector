483eafe47ecd412660732656d85478e0f9991855
howardnguyen714 <howardnguyen714@gmail.com>
2021-03-10 15:48:30 -0800

    implemented postgresql into index.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Game
 
 # Create your views here.
 def home(request):
@@ -9,18 +9,5 @@ def about(request):
     return render(request, 'about.html')
 
 def games_index(request):
+    games = Game.objects.all()
     return render(request, 'games/index.html', { 'games': games })
-
-
-class Game:
-  def __init__(self, title, genre, description, playtime):
-    self.title = title
-    self.genre = genre
-    self.description = description
-    self.playtime = playtime
-
-games = [
-  Game('Lolo', 'tabby', 'foul little demon', 3),
-  Game('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-  Game('Raven', 'black tripod', '3 legged cat', 4)
-]
