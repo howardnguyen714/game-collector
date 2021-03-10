3351af911173ae47222fbd164e23f91be96d794a
howardnguyen714 <howardnguyen714@gmail.com>
2021-03-10 14:28:03 -0800

    added index route that allows user to view all their games

--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,20 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def games_index(request):
+    return render(request, 'games/index.html', { 'games': games })
+
+
+class Game:
+  def __init__(self, title, genre, description, playtime):
+    self.title = title
+    self.genre = genre
+    self.description = description
+    self.playtime = playtime
+
+games = [
+  Game('Lolo', 'tabby', 'foul little demon', 3),
+  Game('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Game('Raven', 'black tripod', '3 legged cat', 4)
+]
