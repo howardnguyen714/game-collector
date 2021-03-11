853fe4c5ca2a1ce75d43fdc5e5e3025d0060d855
howardnguyen714 <howardnguyen714@gmail.com>
2021-03-10 17:43:57 -0800

    added game details page

--- main_app/models.py
+++ main_app/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Game(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
     playtime = models.IntegerField()
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Game
 
 # Create your views here.
@@ -11,3 +12,7 @@ def about(request):
 def games_index(request):
     games = Game.objects.all()
     return render(request, 'games/index.html', { 'games': games })
+
+def games_detail(request, game_id):
+  game = Game.objects.get(id=game_id)
+  return render(request, 'games/detail.html', { 'game': game })
